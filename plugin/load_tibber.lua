6a8d2cf2f7f8751bdf1fef7331a74a0706b4175b
TomannMoritz <164042654+TomannMoritz@users.noreply.github.com>
2026-02-01 13:45:13 +0100

    refactor: user command - query resolution - feat: custom multi argument auto completion

--- plugin/load_tibber.lua
+++ plugin/load_tibber.lua
@@ -1,13 +1,64 @@
--- Set user commands
-vim.api.nvim_create_user_command("TibberToggle", function(opts)
-    local requery = opts.args == "true"
+-- user commands
 
-    require("tibber").toggle_window(requery)
+local white_space = "%s+"
+
+
+--- Filter empty strings in table
+---@param t table
+---@return table filtered_table
+local function filter_empty_str(t)
+    local filtered_table = {}
+
+    for _, str in ipairs(t) do
+        if str ~= "" then
+            table.insert(filtered_table, str)
+        end
+    end
+
+    return filtered_table
+end
+
+
+--- Custom multi argument auto complete function
+---@param completion_list table
+---@return function
+local function custom_complete(completion_list)
+    return function (ArgLead, CmdLine, CursorPos)
+        local args = vim.split(CmdLine, white_space)
+        local arg_pos = #args - 1
+
+        if completion_list == nil then return {} end
+        if #completion_list < arg_pos then return {} end
+
+        return completion_list[arg_pos]
+    end
+end
+
+
+local api_completion_list = {
+    {"HOURLY", "QUARTERLY"},
+    {"true", "false"}
+}
+
+
+-- Create user commands
+vim.api.nvim_create_user_command("TibberToggle", function(opts)
+    local args = vim.split(opts.args, white_space)
+    args = filter_empty_str(args)
+
+    -- first argument: query resolution
+    local resolution = nil
+    if #args >= 1 then
+        resolution = args[1]
+    end
+
+    -- second argument: requery API
+    local requery = #args >= 2 and args[2] == "true"
+
+    require("tibber").toggle_window(resolution, requery)
 end, {
-        nargs = "?", -- zero or one argument
-        complete = function()
-            return { "true", "false" } -- Auto completion suggestions
-        end,
+        nargs = "*", -- zero or many arguments
+        complete = custom_complete(api_completion_list),
         desc = "Toggle floating window with energy prices"
     }
 )
